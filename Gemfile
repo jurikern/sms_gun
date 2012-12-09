51db9fc10d425901617aa4c86600ed8cb902d0df
Juri Semjonov <juri.semjonov@gmail.com>
2012-12-09 23:00:42 +0200

    initial commit

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.9'
+
+group :assets do
+  gem 'sass-rails'
+  gem 'coffee-rails'
+  gem 'therubyracer', :platforms => :ruby
+  gem 'uglifier'
+  gem 'yui-compressor'
+end
+
+group :development do
+  gem 'hpricot'
+  gem 'ruby_parser'
+  gem 'thin'
+end
+
+gem 'jquery-rails'
+gem 'haml-rails'
+gem 'pg'
+gem 'foreigner'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SmsGun::Application.config.secret_token = 'd9444ff2e3d0b7f02c9435cf68698fc9e0c30613ceab4b389b3805815c9d8d4f6f63a2b5fdb1c772dea7a184fab20295ad5245dabdf32e91f71f79c26f304336'
